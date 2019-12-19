533700c74cc33d078b53004c08e888522fd8bb1f
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-12-18 21:01:50 -0500

    writing route

--- router/router.js
+++ router/router.js
@@ -2,47 +2,64 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const { escaper } = require("../util");
 const { find } = require("../mongodb/methods/");
 const { sfrc, hfac, sasc } = require("../mongodb/schemas/committees");
-
-const collPicker = (msg) => {
-  let { Body, From } = msg;
-  let database = (() => {
-    switch(Body.toLowerCase()){
-      case 'senate foreign relations committee':
-        return sfrc
-      case 'senate armed services committee':
-        return sasc
-      case 'house foreign relations committee':
-        return hfac
-      default:
-        return null;
-    };
-  })();
-    return database;
-};
-
-const fetchData = async (db) => {
-  const data = await find(db);
-  return data;
-}
+const moment = require("moment");
+const axios = require("axios");
 
 var appRouter = (app, db) => {
-  app.get("/", (req, res) => {
-    res.status(200)
-      .send("You may have reached this page in error. Please contact Harrison Cramer, the national security correspondent at National Journal.");
-  });
+  
+  const proPublicaApiOptions = { headers: { 'X-API-Key': process.env.PRO_PUBLICA }}; // Set options for Pro Publica API.
+  
+  const handleGetCommittees = async (memUri) => {
+    let { data } = await axios.get(memUri, proPublicaApiOptions);
+    let currentCommittees = data.results[0].roles.filter(role => role.congress == '116' )[0].committees; // .filter((mem) => mem.first_name.toLowerCase() === first && mem.last_name.toLowerCase() === last);
+    return currentCommittees.length > 0 ? currentCommittees.map(committee => committee.code) : null
+  };
+
+  const handleGetUri = async (sms) => {
+    let first = sms.split(" ")[0];
+    let last = sms.split(" ")[1];
+    let { data } = await axios.get("https://api.propublica.org/congress/v1/116/senate/members.json", proPublicaApiOptions);
+    let memberData = data.results[0].members.filter((mem) => mem.first_name.toLowerCase() === first && mem.last_name.toLowerCase() === last);
+    return memberData.length > 0 ? memberData[0].api_uri : null;
+  };
+
+  const handleNoDataFound = async (failMessage, res) => {
+    const twiml = new MessagingResponse();
+    twiml.message(failMessage);
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  };
+
+  const getHearingSked = async (id, theDate) => {
+    console.log(id, theDate);
+    return 'ok';
+  };
 
   app.post("/sms", async(req,res) => {
-
     const msg = req.body;
-    let coll = collPicker(msg) ? collPicker(msg) : "Sorry, that information could not be found.";
+    let { Body, From } = msg;
+    let memUri = await handleGetUri(Body); // Get member's URI (url string)
+    if (!memUri) {
+      return handleNoDataFound('Sorry, that member could not be found.', res);
+    };
 
-    let response = await fetchData(coll);
+    let memCommittees = await handleGetCommittees(memUri); // Get member's committee assignments (list)
+    if (!memCommittees) {
+      return handleNoDataFound('Sorry, that member does not appear to have any committee assignments.', res);
+    };
 
-    // Send escaped to original recipient...
-    const twiml = new MessagingResponse();
-    twiml.message(JSON.stringify(response));
-    res.writeHead(200, {'Content-Type': 'text/xml' });
-    res.end(twiml.toString());
+    let promises = memCommittees.map(async(id) => await getHearingSked(id, moment().format('llll'))); // Get the schedule for each hearing.
+    Promise.all(promises)
+      .then(results => {
+        if(results.length > 0){
+          console.log(results);
+        } else {
+          handleNoDataFound('Sorry, that member does not appear to have any hearings coming up.', res);
+        }
+      })
+      .catch(err => {
+        handleNoDataFound('Sorry, something went wrong with our server.', res);
+      });
   });
 }
 
